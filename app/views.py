b4e873c4cefa340ddf2c3ac3fc8f9ae91b643afc
haricharan9120 <haricharan9120@gmail.com>
2024-04-04 17:17:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def csk(request):
+    return render(request,'csk.html')
+def MAHI(request):
+    return render(request,'MAHI.html')
